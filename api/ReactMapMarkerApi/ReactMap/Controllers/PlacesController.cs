cd086e275c5c88150d7acd4ec26438a5b1b2bf22
Felipe S. Fernandes <feleast.49@gmail.com>
2019-01-30 16:46:04 -0300

    Web Api em AspNetCore

---
+++ api/ReactMapMarkerApi/ReactMap/Controllers/PlacesController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ReactMap.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Places")]
+    public class PlacesController : Controller
+    {
+        [Route("All")]
+        [HttpGet]
+        public IEnumerable<string> Index()
+        {
+            return new string[] { "Places" };
+        }
+    }
+}
---
+++ api/ReactMapMarkerApi/ReactMap/Database/DbHandler.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using NHibernate;
+using FluentNHibernate.Cfg;
+using FluentNHibernate.Cfg.Db;
+
+namespace ReactMap.Database
+{
+    public class DbHandler
+    {
+        public static ISessionFactory CreateSessionFactory()
+        {
+            return Fluently
+                .Configure().Database(MsSqlConfiguration.MsSql2005.ConnectionString(c =>
+                //.Mappings(null)
+                .BuildSessionFactory();
+        }
+    }
+}
